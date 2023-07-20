b8bd08bcf784acce51897f7436d00a2a4a246931
ronokelvin <ronokipyegon079@gmail.com>
2023-07-20 09:24:14 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - 5 functions
+ * @x: input one
+ * @y: input two
+ * Return: 0
+ */
+int op_add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * op_sub - 5 functions
+ * @x: input one
+ * @y: input two
+ * Return: 0
+ */
+int op_sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * op_mul - 5 functions
+ * @x: input one
+ * @y: input two
+ * Return: 0
+ */
+int op_mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * op_div - 5 functions
+ * @x: input one
+ * @y: input two
+ * Return: 0
+ */
+int op_div(int x, int y)
+{
+	return (x / y);
+}
+/**
+ * op_mod - 5 functions
+ * @x: input one
+ * @y: input two
+ * Return: 0
+ */
+int op_mod(int x, int y)
+{
+	return (x % y);
+}
